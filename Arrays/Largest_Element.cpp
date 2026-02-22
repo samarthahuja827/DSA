a974bef87e9b49b8ce619da09fa723d2b68fe72e
Samarth Ahuja <samarthahuja827@gmail.com>
2026-02-22 21:39:11 +0530

    Update Largest_Element.cpp

--- Arrays/Largest_Element.cpp
+++ Arrays/Largest_Element.cpp
@@ -21,7 +21,7 @@ class Solution {
 public:
     int largestElement(vector<int>& nums) {
         int largest=nums[0];
-        for(int i=1;i<=nums.size();i++) {
+        for(int i=1;i<nums.size();i++) {
          if(nums[i]>largest) {largest = nums[i];}
        }
        return largest;
